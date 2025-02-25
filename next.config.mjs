5a994bdeb4353c2ec1c0a8c4ba799a3e8c97d1be
kamron9 <kamronalimov80@gmail.com>
2025-02-25 17:53:04 +0500

    bug fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'admin.giotto-urgench.uz',
-			},
-		],
-	},
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'admin.giotto-urgench.uz',
+      },
+    ],
+  },
 }
 
 export default nextConfig
